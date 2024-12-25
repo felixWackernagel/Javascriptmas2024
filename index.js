387f926916ef7e1ccbccbb685f1dd90cae4c8f58
Felix Wackernagel <felix.wackernagel@gmail.com>
2024-12-25 22:28:03 +0100

    Solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { codedMessage } from './codedMessage.js'
+import { codedMessage } from "./codedMessage.js";
 
 /*
 codedMessage.js holds a coded message (well, the name makes it obvious, huh?).
@@ -10,4 +10,38 @@ key.md will help!
 
 **Stretch Goal**
 No stretch goal for the final day. Just stretch your legs!
-*/ 
+*/
+
+const HINT = 10;
+const codedMessageEl = document.getElementById("codedMessage");
+const encodedMessageEl = document.getElementById("encodedMessage");
+const decodedMessageEl = document.getElementById("decodedMessage");
+
+function binaryToDecimal(asciiBinaryCode) {
+  return parseInt(asciiBinaryCode, 2);
+}
+
+function decimalToCharacter(asciiDecimalCode) {
+  return String.fromCharCode(asciiDecimalCode);
+}
+
+function decode(asciiDecimalCode) {
+  let decimal = asciiDecimalCode - HINT;
+  if (decimal <= 31) {
+    decimal = 127 - (31 - decimal);
+  }
+  return decimal;
+}
+
+codedMessageEl.textContent = codedMessage.join(" ");
+
+encodedMessageEl.textContent = codedMessage
+  .map(binaryToDecimal)
+  .map(decimalToCharacter)
+  .join("");
+
+decodedMessageEl.textContent = codedMessage
+  .map(binaryToDecimal)
+  .map(decode)
+  .map(decimalToCharacter)
+  .join("");
--- key.md
+++ key.md
@@ -1,2 +1,4 @@
 abc = klm
 xyz = "#$
+
+the distance between a to k, b to l, c to m, ... is 10 in ascii decimal numbers
