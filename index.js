c548a62f92e38396857e242d6c9b44c674cbfa8d
Felix Wackernagel <felix.wackernagel@gmail.com>
2024-12-26 23:42:39 +0100

    Solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { addresses } from './addresses.js'
+import { addresses } from "./addresses.js";
 /*
 Writing out labels by hand is a real pain. Luckily, you are so organised that you have all of your contacts saved in an array.
 
@@ -13,4 +13,40 @@ But not all of your contacts are on your Christmas list. So your task is this:
 2. Create your own CSS Christmas logo to add a personal touch to each label.
 */
 
-const labelsContainer = document.querySelector('.labels-container')
+const icons = [
+  "bauble",
+  "bow",
+  "candy-cane",
+  "deer",
+  "gifts",
+  "gingerbread-man",
+  "santa-hat",
+  "santa",
+  "snowflake",
+  "snowglobe",
+  "snowman",
+  "star-bauble",
+  "star",
+  "stocking",
+  "tree",
+  "trees",
+  "wreath",
+];
+
+let one = 0;
+let two = icons.length - 1;
+
+const labelsContainer = document.querySelector(".labels-container");
+
+addresses
+  .filter((address) => address.isOnChristmasList)
+  .forEach((address) => {
+    const icon1 = icons[one];
+    const icon2 = icons[two];
+    one++;
+    two--;
+    const item = document.createElement("div");
+    item.classList.add("label");
+    item.innerHTML = `<span class="name">${address.name}</span>${address["address line 1"]}<br>${address.town}<br>${address.state} / ${address.country}<img class="label__icon--one" src="icons/${icon1}.png"/><img class="label__icon--two" src="icons/${icon2}.png"/><span class="label__logo">SC</span>`;
+    labelsContainer.appendChild(item);
+  });
