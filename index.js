8b662259e866d4cfd309c4f366587d310e4d2c11
Felix Wackernagel <felix.wackernagel@gmail.com>
2024-12-27 01:39:02 +0100

    Solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { toysRequested } from './data.js'
+import { toysRequested } from "./data.js";
 
 /*
 The run up to Christmas is quite a data-intensive time for Santa. In order to understand market trends, Santa's Data Elves have collated sample children’s wish list data from 4 locations and now need to know which was the most popular toy requested overall. This will help with procurement for next year. 
@@ -11,7 +11,34 @@ Expected output: "The most popular toy is 🎲 board games with 9000 requests.""
 **Stretch Goal**
 - Complete this challenge using the .flatMap() method to work with the various "toys" arrays.
 
-*/ 
+*/
 
+function groupByNameAndSum(map, toy) {
+  let name = Object.keys(toy)[0];
+  let sum = map.get(name);
+  let quantity = toy[name];
+  if (sum) {
+    map.set(name, sum + quantity);
+  } else {
+    map.set(name, quantity);
+  }
+  return map;
+}
 
-console.log(`The most popular toy is <TOY> with <NUMBER> requests.`);
+const mapNameToSum = toysRequested
+  .flatMap((data) => data.toys)
+  .reduce(groupByNameAndSum, new Map());
+
+const sortedBySumDesc = [...mapNameToSum.entries()].sort((a, b) => {
+  let sumA = a[1];
+  let sumB = b[1];
+  if (sumA < sumB) return 1;
+  if (sumA > sumB) return -1;
+  return 0;
+});
+
+const bestToy = sortedBySumDesc[0];
+
+console.log(
+  `The most popular toy is ${bestToy[0]} with ${bestToy[1]} requests.`
+);
