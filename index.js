3725e864867a941c24e9751602d11334e209bf92
Felix Wackernagel <felix.wackernagel@gmail.com>
2024-12-29 21:57:15 +0100

    Solution

--- index.js
+++ index.js
@@ -23,38 +23,108 @@ For example, only one of these entries should be added to the list — the other
 */
 
 // Get references to DOM elements
-const itemInput = document.getElementById('item-input')
-const addItemButton = document.getElementById('add-item-button')
-const shoppingList = document.getElementById('shopping-list')
-const listArr = []
+const itemInput = document.getElementById("item-input");
+const addItemButton = document.getElementById("add-item-button");
+const shoppingList = document.getElementById("shopping-list");
+const stopEditButton = document.getElementById("stop-edit-button");
+const listArr = [];
+const MULTIPLE_SPACES = /\s{2,}/g;
+
+function sanitize(text) {
+  return text.trim().toLowerCase().replaceAll(MULTIPLE_SPACES, " ");
+}
 
 // Function to check item is not duplicate
 function checkDuplicate() {
-    
-    /* ⚠️ You need to add code to this function! ⚠️*/ 
-    
-    const itemText = itemInput.value
-    listArr.push(itemText)
-    renderList()
+  /* ⚠️ You need to add code to this function! ⚠️*/
+
+  const isEditMode = document.body.classList.contains("edit-mode");
+  const itemText = itemInput.value;
+
+  if (itemText === null || itemText.trim().length === 0) {
+    if (isEditMode) {
+      stopEditing();
+      alert("Empty input is not allowed.");
+    } else {
+      itemInput.value = "";
+    }
+    return;
+  }
+
+  if (listArr.map(sanitize).includes(sanitize(itemText))) {
+    if (isEditMode) {
+      stopEditing();
+      alert("Duplicate input is not allowed.");
+    } else {
+      itemInput.value = "";
+    }
+    return;
+  }
+
+  let text = itemText.trim().replaceAll(MULTIPLE_SPACES, " ");
+  if (isEditMode) {
+    let origin = itemInput.dataset.edit;
+    let index = listArr.indexOf(origin);
+    listArr[index] = text;
+    stopEditing();
+  } else {
+    listArr.push(text);
+  }
+  renderList();
+}
+
+function removeItem(item) {
+  listArr.splice(listArr.indexOf(item), 1);
+  renderList();
+}
+
+function stopEditing() {
+  itemInput.value = itemInput.dataset.before;
+  itemInput.removeAttribute("data-before");
+  itemInput.removeAttribute("data-edit");
+  document.body.classList.remove("edit-mode");
+}
+
+function editItem(item) {
+  itemInput.dataset.edit = item;
+  itemInput.dataset.before = itemInput.value;
+  itemInput.value = item;
+  document.body.classList.add("edit-mode");
 }
 
 // Function to add an item to the shopping list
 function renderList() {
-    shoppingList.innerHTML = ''
-    listArr.forEach((gift) => {
-        const listItem = document.createElement('li')
-        listItem.textContent = gift
-        shoppingList.appendChild(listItem)
-    })
-    itemInput.value = ''; // Clear the input field
+  shoppingList.innerHTML = "";
+  listArr.forEach((gift) => {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑️";
+    deleteBtn.onclick = () => removeItem(gift);
+
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "✏️";
+    editBtn.onclick = () => editItem(gift);
+
+    const container = document.createElement("div");
+    container.textContent = gift;
+    container.appendChild(editBtn);
+    container.appendChild(deleteBtn);
+
+    const listItem = document.createElement("li");
+    listItem.appendChild(container);
+
+    shoppingList.appendChild(listItem);
+  });
+  itemInput.value = ""; // Clear the input field
 }
 
 // Add event listener to button
-addItemButton.addEventListener('click', checkDuplicate)
+addItemButton.addEventListener("click", checkDuplicate);
+
+stopEditButton.addEventListener("click", stopEditing);
 
 // Allow adding items by pressing Enter key
-itemInput.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        checkDuplicate()
-    }
-})
+itemInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    checkDuplicate();
+  }
+});
